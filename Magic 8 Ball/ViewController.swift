1ae85b1a34ba04ac1f5370659f9745c9b148bb67
nkazakova <natalia.kazakova@innoway.ru>
2019-05-24 14:24:37 +0300

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Natalia Kazakova on 24/05/2019.
+//  Copyright © 2019 Natalia Kazakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
